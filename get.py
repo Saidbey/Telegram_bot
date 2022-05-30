7f8079f5f1e6e2c9b3d8509237bc4240e58e7b0b
Saidbey <104683036+Saidbey@users.noreply.github.com>
2022-05-30 15:25:48 +0500

    creating json file

--- get.py
+++ get.py
@@ -11,16 +11,17 @@ class numpyarrayencoder(JSONEncoder):
         return JSONEncoder.default(self, obj)
 
 data = []
-for s in os.listdir('Topshiriq2'):
+# Coming steps you should give the adress of the pictures, should be encoded
+for s in os.listdir('Topshiriq2'):# This code enters the folders located in Topshiriq2 folder
     # print(s)
-    for a in os.listdir(f'Topshiriq2/{s}'):
+    for a in os.listdir(f'Topshiriq2/{s}'): # There three folders in Topshiriq2 and this code enters every single one and takes pictures from them
         # print(s, a)
-        name = a.split(".")[0]
+        name = a.split(".")[0]#Pictuers in folders is in like someone.jpg and this code split them and takes just the name of the picture(someone)
         # print(name)
-        path = f"Topshiriq2/{s}/{a}"
+        path = f"Topshiriq2/{s}/{a}"#This is all path
         # print(path)
         pic = face_recognition.load_image_file(path)
-        encode_pic = face_recognition.face_encodings(pic)[0]
+        encode_pic = face_recognition.face_encodings(pic)[0]#This encodes all the pictures
 #
         i = {'name':name,
              'path':path,
